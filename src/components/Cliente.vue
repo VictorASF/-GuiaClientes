566c97c7f7d746659e9baefe3eb36371cabc5df5
Victor Fonseca <vasf111@hotmail.com>
2021-03-29 18:29:57 -0300

    Inicial

---
+++ src/components/Cliente.vue
@@ -0,0 +1,37 @@
+<template>
+<div id='cliente'>
+    <h2 id='cliente-nome'>{{nome}}</h2>
+    <h2>{{idade}} anos </h2>
+    <h3>
+        Descrição do cliente
+    </h3>
+    {{
+        2+2*2
+    }}
+</div>
+    
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            nome:'Victor',
+            numero:'',
+            email:'',
+            idade:21
+        }
+    }
+}   
+</script>
+
+<style scoped>
+    #cliente{
+        color:blue;
+        max-width: 600px;
+
+    }
+    #cliente-nome{
+        color:blue;
+    }
+</style>
---
+++ src/components/Produto.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+    <h3>Carregador</h3>
+    <p>CAralhoooooo</p>
+    <input type="number" placeholder="Preço">    
+</div>    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
