99b098deaa60d52ebcc0a7b2783af16afeeee223
Victor Fonseca <vasf111@hotmail.com>
2021-03-30 19:16:41 -0300

    Aula 2

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,34 @@
 <template>
   <div id="app">
     <h1>Guia Clientes</h1>
-    <Cliente/>
-    <Produto/>
+    <Cliente :client='clients[0]' />
+    <Cliente :client='clients[1]' />
   </div>
 </template>
 
 <script>
 import Cliente from "./components/Cliente"
-import Produto from "./components/Produto"
 
 export default {
   name: 'App',
+  data(){
+    return{
+      clients:[
+        {
+          nome: "Victor Fonseca",
+          email: "teste@victor.com",
+          idade: 21
+        },
+        {
+          nome: "Anne Badu",
+          email: "teste@anne.com",
+          idade: 22
+        }
+      ]
+    }
+  },
   components:{
-    Cliente,
-    Produto,
+    Cliente
   }
 }
 </script>
--- src/components/Cliente.vue
+++ src/components/Cliente.vue
@@ -1,13 +1,10 @@
 <template>
 <div id='cliente'>
-    <h2 id='cliente-nome'>{{nome}}</h2>
-    <h2>{{idade}} anos </h2>
-    <h3>
-        Descrição do cliente
-    </h3>
-    {{
-        2+2*2
-    }}
+    <h4>Nome: {{client.nome}}</h4>
+    <hr>
+    <p>Email: {{ client.email }}</p>
+    <p v-if="client.idade >= 18">Idade: {{ client.idade }}</p>
+    <p v-else>Cliente menor de 18 anos</p>
 </div>
     
 </template>
@@ -16,22 +13,20 @@
 export default {
     data(){
         return{
-            nome:'Victor',
-            numero:'',
-            email:'',
-            idade:21
         }
+    },
+    props: {
+        client: Object,
+        showAge: Boolean
     }
 }   
 </script>
 
 <style scoped>
     #cliente{
+        background-color: #ECE5E3;
         color:blue;
-        max-width: 600px;
-
-    }
-    #cliente-nome{
-        color:blue;
+        padding: 1%;
+        margin-top: 2%;
     }
 </style>
